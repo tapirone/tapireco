c28dea3bb87de68a50ec87ffbef413b90d7913ed
Tapirone <tapironememe@gmail.com>
2025-04-23 15:18:30 +0700

    remove phantom, solflare only in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,32 @@
 import '@/styles/globals.css';
-import dynamic from 'next/dynamic';
-import { useMemo } from 'react';
-import Navbar from '../components/Navbar';
-
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import {
   ConnectionProvider,
   WalletProvider
 } from '@solana/wallet-adapter-react';
-import {
-  WalletModalProvider
-} from '@solana/wallet-adapter-react-ui';
-import {
-  PhantomWalletAdapter
-} from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { useMemo } from 'react';
 import '@solana/wallet-adapter-react-ui/styles.css';
 
-const endpoint = clusterApiUrl('mainnet-beta');
+function MyApp({ Component, pageProps }) {
+  const network = WalletAdapterNetwork.Mainnet;
+  const endpoint = 'https://api.mainnet-beta.solana.com';
 
-const App = ({ Component, pageProps }) => {
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+  const wallets = useMemo(
+    () => [new SolflareWalletAdapter()],
+    []
+  );
 
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>
-          <Navbar />
           <Component {...pageProps} />
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   );
-};
+}
 
-export default dynamic(() => Promise.resolve(App), {
-  ssr: false
-});
+export default MyApp;
